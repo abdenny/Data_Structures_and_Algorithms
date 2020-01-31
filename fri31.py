1874f1ed20d99a38d34fe5d7b04bd94711692477
Austin Denny <austinbdenny@gmail.com>
2020-01-31 10:42:03 -0600

    First class algos.

---
+++ fri31.py
@@ -0,0 +1,27 @@
+# def check_triplet(string1, b, c):
+#     return (a**2) + (b**2) == (c**2)
+
+
+# limit = 1000
+
+# for x in range(1, limit):
+#     for y in range(x+1, limit):
+#         z = limit - (x + y)
+#         if(check_triplet(x, y, z)):
+#             if x + y + z == limit:
+#                 print(x)
+#                 print(y)
+#                 print(z)
+#                 print(x * y * z)
+#                 break
+
+# list1 = []
+# list2 = []
+# string1 = input("Input a string of characters... ")
+# string2 = string1 + input("Enter an additional character... ")
+# for i in string1:
+#     list1.append(i)
+# for i in string2:
+#     list2.append(i)
+# if len(list2) > len(list1):
+#     print(list2[len(list1)])
