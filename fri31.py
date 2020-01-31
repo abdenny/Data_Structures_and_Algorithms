c306ae12ff0d1c3c20a9b8014c4232cb3d8b2ba8
Austin Denny <austinbdenny@gmail.com>
2020-01-31 10:54:15 -0600

    Fixed.

--- fri31.py
+++ fri31.py
@@ -1,20 +1,24 @@
-# def check_triplet(string1, b, c):
-#     return (a**2) + (b**2) == (c**2)
+
+# 2.
 
 
-# limit = 1000
+def check_triplet(a, b, c):
+    return (a**2) + (b**2) == (c**2)
 
-# for x in range(1, limit):
-#     for y in range(x+1, limit):
-#         z = limit - (x + y)
-#         if(check_triplet(x, y, z)):
-#             if x + y + z == limit:
-#                 print(x)
-#                 print(y)
-#                 print(z)
-#                 print(x * y * z)
-#                 break
 
+limit = 1000
+
+for x in range(1, 1001):
+    for y in range(x+1, 1001):
+        z = limit - (x + y)
+        if(check_triplet(x, y, z)):
+            if x + y + z == limit:
+                print(x)
+                print(y)
+                print(z)
+                print(x * y * z)
+                break
+# 3.
 # list1 = []
 # list2 = []
 # string1 = input("Input a string of characters... ")
